727128c059f875fb7b17ca6d6eb95c92cfc75421
Stanisław Kępiński <kepinskiei19@gmail.com>
2023-03-30 13:59:24 +0200

    add node, mysql2 and test code

---
+++ db/index.js
@@ -0,0 +1,19 @@
+const connection = require("mysql2").createConnection({
+    host: "localhost",
+    user: "stahoo",
+    password: "",
+    database: "projekt",
+  });
+  
+async function odpytaj(komenda) {
+    connection
+        .promise()
+        .query(komenda)
+        .then(function ([results, fields]) {
+            console.log(results);
+        })
+        .catch(console.log);
+}
+odpytaj("SELECT * FROM test1");
+  
+connection.end();
