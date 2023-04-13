a181a6299068e0d24c5664b4eef98304077559a9
Stanisław Kępiński <kepinskiei19@gmail.com>
2023-04-13 14:00:20 +0200

    add some errors

--- db/index.js
+++ db/index.js
@@ -3,14 +3,14 @@ const connection = require("mysql2").createConnection({
     user: "stahoo",
     password: "",
     database: "projekt",
-  });
+    });
   
 async function odpytaj(komenda) {
     connection
         .promise()
         .query(komenda)
         .then(function ([results, fields]) {
-            return results;
+            console.log(results);
         })
         .catch(console.log);
 }
@@ -19,14 +19,16 @@ const axios = require('axios');
 
 axios.get('http://imiki.pl/cf')
   .then(response => {
-    przetworzJson(response.data);
+    przetworzJson(response);
   })
   .catch(error => {
     console.log(error);
   });
 
 function przetworzJson(dane)    {
-    console.log(dane.data[3]);
+    //console.log(dane.data[3]);  //index 3 to temperatura, index 2 to wilgotnosc
+
+    odpytaj("INSERT INTO termometr (id_odczytu,wartosc,kiedy_pobrano) values (NULL, '69', '2069-01-01 21:37:00')")
 }
 
 connection.end();
