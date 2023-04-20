5261cce818864c876a4c91922966a6d78988992b
Grupa Zdalny2 <kepinskiei19@gmail.com>
2023-04-20 12:33:42 +0000

    minor adjustments

--- db/index.js
+++ db/index.js
@@ -4,10 +4,10 @@ function wciskajDoBazy(dane,ktorySensor) {
     let query;
 
     if(ktorySensor == 2) {
-        query = 'INSERT INTO wilgotnosc (id, wartosc, kiedy_zczytano) VALUES (NULL, "'+dane.data[ktorySensor].state.humidity+'", "'+dane.timeStamp.date+'");';
+        query = 'INSERT INTO wilgotnosc (id, wartosc, kiedy_pobrano) VALUES (NULL, "'+dane.data[ktorySensor].state.humidity+'", "'+dane.timeStamp.date+'");';
     }
     else if (ktorySensor == 3)   {
-        query = 'INSERT INTO termometr (id_odczytu, wartosc, kiedy_pobrano) VALUES (NULL, "'+dane.data[ktorySensor].state.temperature+'", "'+dane.timeStamp.date+'");';
+        query = 'INSERT INTO termometr (id, wartosc, kiedy_pobrano) VALUES (NULL, "'+dane.data[ktorySensor].state.temperature+'", "'+dane.timeStamp.date+'");';
     }
 
     const connection = require("mysql2").createConnection({
@@ -28,7 +28,8 @@ function wciskajDoBazy(dane,ktorySensor) {
     connection.end();
 }
 
-axios.get('http://imiki.pl/cf')
+
+    axios.get('http://192.168.88.254/cf')
         .then(response => {
             for (let i = 2; i<=3; i++)  {
                 wciskajDoBazy(response.data,i);
